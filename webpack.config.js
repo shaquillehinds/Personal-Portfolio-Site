aaad84ff36710fb4c1dca41f45edf2a19f5c9cd6
Shaq <shaqdulove@hotmail.com>
2020-05-04 23:28:44 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devtool: "eval-cheap-module-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    port: 3000,
+  },
+};
